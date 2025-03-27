33e89557322714de524c7d679cd23e9ad450db88
DESKTOP-TLGP5B5\sergio <sergiocupa@outlook.com>
2025-03-27 00:55:52 -0300

    implementacao inicial

---
+++ ObjectCloner/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ObjectCloner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var gg = new ObjetoTestePai()
+            {
+                ID = 5555,
+                Parent = new ObjetoTestePai() { ID = 6666 },
+                //Atributo = new ObjetoTesteFilho() { ID = 123, Name = "gggg" },
+                //ChildArray = new[]
+                //{
+                //    new ObjetoTesteFilho() { ID = 456, Name = "ABC..." }
+                //},
+                //Arrays = new[]
+                //{
+                //    new ObjetoTestePai() { ID = 789, Parent = new ObjetoTestePai(){ ID = 999 } }
+                //},
+                Children = new List<ObjetoTestePai>()
+            };
+            gg.Children.Add(new ObjetoTestePai() { ID = 7777 });
+
+            // Como pode ter instancias circular, testar instancia ativa.
+            // Para cada propriedade, registrar os niveis de reentrada.
+            // Como resolver referencia circular, por lista???
+
+            var copy = Cloner.Clone(gg);
+
+            string jj = "";
+        }
+    }
+
+
+
+    public class ObjetoTesteFilho
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ObjetoTestePai
+    {
+        public int ID { get; set; }
+        //public ObjetoTesteFilho Atributo { get; set; }
+        public ObjetoTestePai Parent { get; set; }
+        public List<ObjetoTestePai> Children { get; set; }
+        //public ObjetoTesteFilho[] ChildArray { get; set; }
+        //public ObjetoTestePai[] Arrays { get; set; }
+    }
+}
