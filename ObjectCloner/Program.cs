9adc41995758b83f3540fc5dd6779a43d05db59a
DESKTOP-TLGP5B5\sergio <sergiocupa@outlook.com>
2025-03-27 13:53:44 -0300

    correcao propriedade lista nao circular

--- ObjectCloner/Program.cs
+++ ObjectCloner/Program.cs
@@ -6,24 +6,11 @@
         {
             var gg = new ObjetoTestePai()
             {
-                ID = 5555,
-                Parent = new ObjetoTestePai() { ID = 6666 },
-                //Atributo = new ObjetoTesteFilho() { ID = 123, Name = "gggg" },
-                //ChildArray = new[]
-                //{
-                //    new ObjetoTesteFilho() { ID = 456, Name = "ABC..." }
-                //},
-                //Arrays = new[]
-                //{
-                //    new ObjetoTestePai() { ID = 789, Parent = new ObjetoTestePai(){ ID = 999 } }
-                //},
-                Children = new List<ObjetoTestePai>()
+                Children = new(),
+                ID = 5555
             };
-            gg.Children.Add(new ObjetoTestePai() { ID = 7777 });
+            gg.Children.Add(new () { ID = 7777 });
 
-            // Como pode ter instancias circular, testar instancia ativa.
-            // Para cada propriedade, registrar os niveis de reentrada.
-            // Como resolver referencia circular, por lista???
 
             var copy = Cloner.Clone(gg);
 
@@ -42,10 +29,7 @@
     public class ObjetoTestePai
     {
         public int ID { get; set; }
-        //public ObjetoTesteFilho Atributo { get; set; }
-        public ObjetoTestePai Parent { get; set; }
         public List<ObjetoTestePai> Children { get; set; }
-        //public ObjetoTesteFilho[] ChildArray { get; set; }
-        //public ObjetoTestePai[] Arrays { get; set; }
     }
+
 }
